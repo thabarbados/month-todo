87c22fde02eb3b810bf7632b351bd0e902435a69
v.terekhov <v.terekhov@iportal.ru>
2020-04-07 21:43:26 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import HelloWorld from './components/HelloWorld';
 
 import './App.css'
 
-@Component
+@Component 
 export default class App extends Vue {
   render() {
     return (
       <div id="app">
         <img alt="Vue logo" src={require('./assets/logo.png')} />
-        <HelloWorld msg1="Welcome to Your Vue.js + TypeScript App"/>
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
       </div>
     )
   }
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -13,13 +13,20 @@ export default class HelloWorld extends VueComponent<Props> {
   @Prop()
   private msg!: string;
 
+  mounted() {
+    console.log(this.getDate());
+  }
+
+  getDate() {
+    const date = new Date()
+    console.log(date.getMonth() + 1);
+    return (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate());
+  }
+
   render() {
     return (
       <div class={styles.hello}>
         <h1>{ this.msg }</h1>
-        <p>
-          ...
-        </p>
       </div>
     )
   }
